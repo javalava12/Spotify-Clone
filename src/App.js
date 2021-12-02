df0852db351504bff31b53f56e1bb70926a3e560
Avantika <avantikaab1@gmail.com>
2021-12-02 15:38:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SpotifyWebApi from "spotify-web-api-js";
 import { useStateValue } from "./StateProvider";
 import Player from "./Player";
 import { getTokenFromResponse } from "./spotify";
-import "./App.css";
+//import "./App.css";
 import Login from "./Login";
 
 
