c67c037893bc97b348a1f09cb25a782a0d391514
Avantika <avantikaab1@gmail.com>
2021-12-02 19:12:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         token: _token,
       });
 
-      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      s.getPlaylist("37i9dQZEVXcKT9awTcdbiZ").then((response) =>
         dispatch({
           type: "SET_DISCOVER_WEEKLY",
           discover_weekly: response,
